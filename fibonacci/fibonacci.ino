35af1c28c4c5335db075a0bf7feb7654eabd70f0
Jamison Hunter <hunterjamison6@gmail.com>
2023-11-21 14:15:56 -0500

    Reformatted

--- fibonacci/fibonacci.ino
+++ fibonacci/fibonacci.ino
@@ -5,24 +5,29 @@ void setup() {
   pinMode(ledPin, OUTPUT);
 }
 
+// Function prototype
+int fibonacci(int num);
+
 void loop() {
   int n = 10; // Change this value to set the number of Fibonacci terms to print
 
   for (int i = 0; i < n; ++i) {
-    Serial.print(fibonacci(i));
+    int fibValue = fibonacci(i);
+    Serial.print(fibValue);
     Serial.print(" ");
     delay(2000);
-    for(int x=x; x>=fibonacci(i); ++x) {
+    for (int x = 0; x < fibValue; ++x) {
       digitalWrite(ledPin, HIGH);
       delay(500);
       digitalWrite(ledPin, LOW);
+      delay(500); // Add an additional delay to control the blink speed
     }
   }
 
   delay(5000); // Delay for 5 seconds before printing again
 }
 
-// Function to calculate Fibonacci value using recursion
+// Function definition
 int fibonacci(int num) {
   if (num <= 1) {
     return num;

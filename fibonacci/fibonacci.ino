4a9d8bfb517e2969164e5d4a61d7d683850f2a16
Jamison Hunter <126278717+JamisonHunter@users.noreply.github.com>
2023-11-25 22:15:40 -0500

    Update fibonacci.ino

--- fibonacci/fibonacci.ino
+++ fibonacci/fibonacci.ino
@@ -5,7 +5,6 @@ void setup() {
   pinMode(ledPin, OUTPUT);
 }
 
-<<<<<<< HEAD
 void loop() {
   int flist[] = {1, 1, 2};
 
@@ -33,35 +32,4 @@ void loop() {
     // Do nothing
   }
 
-=======
-// Function prototype
-int fibonacci(int num);
-
-void loop() {
-  int n = 10; // Change this value to set the number of Fibonacci terms to print
-
-  for (int i = 0; i < n; ++i) {
-    int fibValue = fibonacci(i);
-    Serial.print(fibValue);
-    Serial.print(" ");
-    delay(2000);
-    for (int x = 0; x < fibValue; ++x) {
-      digitalWrite(ledPin, HIGH);
-      delay(500);
-      digitalWrite(ledPin, LOW);
-      delay(500); // Add an additional delay to control the blink speed
-    }
-  }
-
-  delay(5000); // Delay for 5 seconds before printing again
-}
-
-// Function definition
-int fibonacci(int num) {
-  if (num <= 1) {
-    return num;
-  } else {
-    return fibonacci(num - 1) + fibonacci(num - 2);
-  }
->>>>>>> 1fd48e74e320d83a29d4cf6bba1ed7eebd64f861
 }
